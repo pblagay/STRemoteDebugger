1bd807ef4902bd2f53eada34a5503eec446d850b
pblagay <pblagay@gmail.com>
2021-02-19 12:54:57 -0700

    added new form ready for preferences

---
+++ STRemoteDebugger/Preferences.h
@@ -0,0 +1,93 @@
+#pragma once
+
+#include "Debugger.h"
+#include "Helpers.h"
+
+extern STDebugger* g_STDebugger;
+
+namespace CppCLRWinformsSTDebugger 
+{
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Zusammenfassung für Form1
+	/// </summary>
+	public ref class Preferences : public System::Windows::Forms::Form
+	{
+	public:
+		Preferences(void)
+		{
+			InitializeComponent();
+//			void* thisPtr = System::Runtime::InteropServices::GCHandle::ToIntPtr(System::Runtime::InteropServices::GCHandle::Alloc(this)).ToPointer();
+//			g_STDebugger->Init(thisPtr);
+		}
+
+	protected:
+		bool	MemoryWindowInsertModeOn = false;
+
+		/// <summary>
+		/// Verwendete Ressourcen bereinigen.
+		/// </summary>
+		~Preferences()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}	
+	protected:
+
+	private: System::Windows::Forms::ErrorProvider^ errorProvider2;
+	private: System::Windows::Forms::ErrorProvider^ errorProvider1;		
+	private: System::ComponentModel::IContainer^ components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Erforderliche Methode für die Designerunterstützung.
+		/// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->components = (gcnew System::ComponentModel::Container());
+			this->errorProvider1 = (gcnew System::Windows::Forms::ErrorProvider(this->components));
+			this->errorProvider2 = (gcnew System::Windows::Forms::ErrorProvider(this->components));
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->errorProvider1))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->errorProvider2))->BeginInit();
+			this->SuspendLayout();
+
+			// 
+			// 
+			// errorProvider1
+			// 
+			this->errorProvider1->ContainerControl = this;
+			// 
+			// errorProvider2
+			// 
+			this->errorProvider2->ContainerControl = this;
+			// 
+			// Preferences
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(9, 20);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(428, 244);
+			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::Fixed3D;
+			this->Margin = System::Windows::Forms::Padding(4, 5, 4, 5);
+			this->Name = L"Preferences";
+			this->Text = L"Preferences";
+			this->KeyPress += gcnew System::Windows::Forms::KeyPressEventHandler(this, &Preferences::Form1_KeyPress);
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->errorProvider1))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->errorProvider2))->EndInit();
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+#pragma endregion
+	   
+};
+}
